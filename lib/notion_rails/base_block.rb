a57bc5e5133833bbfc62beed7f152011ba0a143e
Guillermo Aguirre <guillermoaguirre@hey.com>
2024-08-24 16:21:02 -0300

    Be able to add custom css classes to video and image

--- lib/notion_rails/base_block.rb
+++ lib/notion_rails/base_block.rb
@@ -67,8 +67,8 @@ module NotionRails
       when 'quote' then render_quote(rich_text, class: options[:quote])
       when 'callout' then render_callout(rich_text, icon, class: options[:callout])
       when 'code' then render_code(rich_text, class: "#{options[:code]} language-#{@properties["language"]}")
-      when 'image' then render_image(*multi_media)
-      when 'video' then render_video(*multi_media)
+      when 'image' then render_image(*multi_media, class: options[:image])
+      when 'video' then render_video(*multi_media, class: options[:video])
       else
         'Error'
       end
